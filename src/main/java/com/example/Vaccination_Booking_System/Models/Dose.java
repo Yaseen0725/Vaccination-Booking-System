fb8df095fe2763cb92470edfbc542b5ac6348c18
Mohd-Yaseen <mohdyaseen2856@gmail.com>
2025-02-12 22:00:27 +0530

    Added the code of model layer

---
+++ src/main/java/com/example/Vaccination_Booking_System/Models/Dose.java
@@ -0,0 +1,59 @@
+package com.example.Vaccination_Booking_System.Models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "dose")
+public class Dose {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String doseId;
+
+    @CreationTimestamp
+    private Date vaccinationDate;
+
+    @OneToOne
+    @JoinColumn
+    @JsonIgnore
+    private User user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDoseId() {
+        return doseId;
+    }
+
+    public void setDoseId(String doseId) {
+        this.doseId = doseId;
+    }
+
+    public Date getVaccinationDate() {
+        return vaccinationDate;
+    }
+
+    public void setVaccinationDate(Date vaccinationDate) {
+        this.vaccinationDate = vaccinationDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/example/Vaccination_Booking_System/Models/VaccinationCenter.java
@@ -0,0 +1,32 @@
+package com.example.Vaccination_Booking_System.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "VaccinationCenter")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VaccinationCenter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String centreName;
+    private LocalTime openingTime;
+    private LocalTime closingTime;
+    private String address;
+    private int doseCapacity;
+
+    @OneToMany(mappedBy = "VaccinationCenter", cascade = CascadeType.ALL)
+    private List<Doctor> doctorList = new ArrayList<>();
+
+}
